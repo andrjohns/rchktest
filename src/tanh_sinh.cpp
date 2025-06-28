76c3982e45010f6292058600a671637b402c5e60
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-06-28 14:05:32 +0800

    test

--- src/tanh_sinh.cpp
+++ src/tanh_sinh.cpp
@@ -2,14 +2,17 @@
 #include <boost/math/quadrature/tanh_sinh.hpp>
 #include <cmath>
 
+double res() {
+  auto func = [](double x) { return std::log(x) * std::log1p(-x); };
+
+  boost::math::quadrature::tanh_sinh<double> integrator;
+  return integrator.integrate(func, 0, 1);
+}
+
 extern "C" {
   SEXP tanh_sinh_() {
     BEGIN_RCPP
-
-    auto func = [](double x) { return std::log(x)*std::log1p(-x); };
-
-    boost::math::quadrature::tanh_sinh<double> integrator;
-    double result = integrator.integrate(func, 0, 1);
+    double result = res();
 
     return Rcpp::wrap(result);
 
